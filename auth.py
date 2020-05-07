197d8bac0455e003a4a6a67dff3e58d4a4a6b289
m-maher <m.maher9@outlook.com>
2020-05-08 00:26:16 +0200

    edit

--- auth.py
+++ auth.py
@@ -76,14 +76,14 @@ def verify_decode_jwt(token):
 
     rsa_key = {}
     for key in jwks['keys']:
-        if key['kid'] == unverified_header['kid']
-        rsa_key = {
-            'kty': key['kty'],
-            'kid': key['kid'],
-            'use': key['use'],
-            'n': key['n'],
-            'e': key['e']
-        }
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
 
     if rsa_key:
         try:
