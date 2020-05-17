05d1d017738a426bd8657933073d6d49835c1645
m-maher <m.maher9@outlook.com>
2020-05-17 20:33:42 +0200

    edit

--- models.py
+++ models.py
@@ -4,13 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 from datetime import date
 
-database_path = 'postgresql://postgres:postgres@localhost:5432/movies'
-
 db = SQLAlchemy()
 
-
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
